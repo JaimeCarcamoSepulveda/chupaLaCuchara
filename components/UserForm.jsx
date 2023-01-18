0cd6c7ed31f6977b4d8aaec89f3318e11d4b4d9e
JaimeCarcamoSepulveda <jaimecarcamobroker@gmail.com>
2023-01-17 22:30:36 -0800

    Revise form and add more fields for user input
    Complete textarea, Checkbox and options section for form. Include submit
    button to later connect with db API

--- components/UserForm.jsx
+++ components/UserForm.jsx
@@ -3,20 +3,33 @@ import React, { useState } from 'react';
 
 export default function UserForm () {
     const [formData, setFormData] = React.useState(
-        { firstName: "", lastName: "", email: "", comments: ""}
+        { firstName: "", 
+        lastName: "",
+         email: "", 
+         comments: "",
+         likePage: true,
+         recommend: "",
+         favPart: ""
+        }
     )
 
     function handleInput(event) {
+        const {name, value, type, checked} = event.target
         setFormData(prevFormData =>{
             return {
                 ...prevFormData,
-                [event.target.name]: event.target.value
+                [name]: type === "checkbox"? checked : value
             }
         })
     }
+    function handleSubmit(event){
+        event.preventDefault() // this will prevent from page being refreshed automatiicaly
+        console.log('submit this form to API db')
+        console.log(formData)
+    }
 
         return (
-            < form >
+            < form onSubmit={handleSubmit}>
                 <input 
                     type="text"
                     placeholder="first name"
@@ -40,15 +53,78 @@ export default function UserForm () {
                      value={formData.email}
                   />
 
-                   <input 
+                   <textarea 
                     className='comment' 
-                     type="text"
-                     placeholder='comments'
+                     placeholder='enter comments here...'
                      onChange={handleInput}
                      name='comments'
                      value={formData.comments}
                   />
-                  <button type="submit">submit</button>
+                <div>
+                    <input 
+
+                        type="checkbox"
+                        checked={formData.likePage}
+                        onChange={handleInput}
+                        id="likePage" 
+                        name='likePage'
+                      />
+                    <label 
+                    htmlFor="likePage"
+                    >I like this page?</label>
+                </div>
+                <fieldset>
+                    <legend>Would you recommend this page to your friends</legend>
+                    <input 
+                        type="radio"
+                        id='yes'
+                        name='recommend'
+                        value="yes"
+                        onChange={handleInput}
+                        checked={formData.recommend === "yes"}
+                     />
+                         <label htmlFor="yes">yes I would</label>
+                     <br />
+                         <input 
+                        type="radio"
+                        id='no'
+                        name='recommend'
+                        value="no"
+                        onChange={handleInput}
+                        checked = {formData.recommend=== "no"}
+                     />
+                     <label htmlFor="no">no, I wouldn't</label>
+                     <br />
+                     <input 
+                        type="radio"
+                        id='maybe'
+                        name='recommend'
+                        value="maybe"
+                        onChange={handleInput}
+                        checked={formData.recommend === "maybe"}
+                     />
+                     <label htmlFor="maybe">Maybe, not so sure yet</label>
+
+
+                </fieldset>
+                <label htmlFor="favPart">What was your favorite part of this page?</label>
+                <br />
+                <select 
+                 name="favPart"
+                 id="favPart"
+                 value={formData.favPart}
+                 onChange={handleInput}
+                 >
+                    <option value="">--choose--</option>
+                    <option value="recipes">recipes</option>
+                    <option value="ingredients">ingredients list</option>
+                    <option value="foodPics">The pictures of food</option>
+                    <option value="variety">the vast variety of recipes</option>
+
+                </select>
+
+
+                  <button>submit</button>
             </form>
         )
 }
